c59aba9b0a01b90e6882c3c1a551ee37dfeacc3f
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-08-25 20:06:58 +0300

    iki 8

--- Kintamieji.php
+++ Kintamieji.php
@@ -56,3 +56,113 @@ echo '<br>'; echo '<br>'; echo '<br>';
 
 
 echo '5. ';
+$skaicius1 = rand (0, 2);
+$skaicius2 = rand (0, 2);
+$skaicius3 = rand (0, 2);
+$skaicius4 = rand (0, 2);
+$nulis = 0;
+$vienetas = 0;
+$dvejetas = 0;
+if ($skaicius1 == 0){
+    $nulis++;
+}
+if ($skaicius2 == 0){
+    $nulis++;
+}
+if ($skaicius3 == 0){
+    $nulis++;
+}
+if ($skaicius4 == 0){
+    $nulis++;
+}
+if ($skaicius1 == 1){
+    $vienetas++;
+}
+if ($skaicius2 == 1){
+    $vienetas++;
+}
+if ($skaicius3 == 1){
+    $vienetas++;
+}
+if ($skaicius4 == 1){
+    $vienetas++;
+}
+if ($skaicius1 == 2){
+    $dvejetas++;
+}
+if ($skaicius2 == 2){
+    $dvejetas++;
+}
+if ($skaicius3 == 2){
+    $dvejetas++;
+}
+if ($skaicius4 == 2){
+    $dvejetas++;
+}
+echo $skaicius1 . ', '. $skaicius2 . ', ' . $skaicius3 . ', ' . $skaicius4 . '.';
+echo '  Nuliu: ' . $nulis . ', ' . 'Vienetu: ' . $vienetas . ', '. 'Dvejetu: ' . $dvejetas;
+
+echo '<br>'; echo '<br>'; echo '<br>';
+
+
+echo '6. ';
+$skaicius = rand (1, 6);
+echo '<h' . $skaicius . '>' . $skaicius . '</h' . $skaicius . '>';
+
+echo '<br>'; echo '<br>'; echo '<br>';
+
+
+echo '7. ';
+$skaicius1 = rand (-10, 10);
+$skaicius2 = rand (-10, 10);
+$skaicius3 = rand (-10, 10);
+if ($skaicius1 < 0){
+    echo '<span style="Color:green">'.$skaicius1.'</span>';
+}
+elseif ($skaicius1 == 0) {
+    echo '<span style="Color:red">'.$skaicius1.'</span>';
+}
+else {
+    echo '<span style="Color:blue">'.$skaicius1.'</span>';
+}
+
+if ($skaicius2 < 0){
+    echo '<span style="Color:green">'.$skaicius2.'</span>';
+}
+elseif ($skaicius2 == 0) {
+    echo '<span style="Color:red">'.$skaicius2.'</span>';
+}
+else {
+    echo '<span style="Color:blue">'.$skaicius2.'</span>';
+}
+
+if ($skaicius3 < 0){
+    echo '<span style="Color:green">'.$skaicius3.'</span>';
+}
+elseif ($skaicius3 == 0) {
+    echo '<span style="Color:red">'.$skaicius3.'</span>';
+}
+else {
+    echo '<span style="Color:blue">'.$skaicius3.'</span>';
+}
+
+// $Color = "red";
+// $Text = "This text is red";
+
+// echo '<div style="Color:'.$Color.'">'.$Text.'</div>';
+// echo '<span style="Color:red">This text is red</span>';
+echo '<br>'; echo '<br>'; echo '<br>';
+
+
+echo '8. ';
+$zvake = rand(5,3000);
+$kaina = $zvake * 1;
+if ($kaina>2000) {
+    $kainaFinale = $kaina - 4;
+}
+elseif ($kaina>1000) {
+    $kainaFinale = $kaina - 3;
+}
+
+echo $zvake.': vnt. - kaina: '.$kainaFinale;
+
