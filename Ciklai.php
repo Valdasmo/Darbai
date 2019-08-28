bed3722b6c59e6d8f0a37d363632560a51358cba
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-08-28 18:39:58 +0300

    +8

--- Ciklai.php
+++ Ciklai.php
@@ -62,16 +62,90 @@ echo '4. ';
 // Nupieškite kvadratą iš “*”, kurio kraštines sudaro 100 “*”. 
 // Panaudokite css stilių, kad kvadratas ekrane atrodytų kvadratinis.
 
-for ($i=0; $i <= 100; $i++) { 
+for ($i=1; $i <= 100; $i++) { 
     echo '<br>';
-    for ($j=0; $j <= 100; $j++){
-        if ($i == $j) {
-            echo '<span style="margin: 0px 1.5px 0px 1.5px; font-size: 5px; Color:red">*</span>';
-        }
-    echo '<span style="margin: 0px 1.5px 0px 1.5px; font-size: 5px;">*</span>';
+    for ($j=1; $j <= 100; $j++){
+        if ($i == $j || $i+$j == 101) {
+        echo '<span style="margin: 0px 1.5px 0px 1.5px; font-size: 10px; Color:red">*</span>';
+        continue;
+            }
+    echo '<span style="margin: 0px 1.5px 0px 1.5px; font-size: 10px;">*</span>';
     }
 }
 echo '<br>'; echo '<br>'; echo '<br>';
 
 
 echo '5. ';
+echo '<br>';
+echo 'Padaryta 4 užduotyje';
+//Prieš tai nupieštam kvadratui nupieškite raudonas istrižaines.
+//Padaryta 4. uzduotyje.
+echo '<br>'; echo '<br>'; echo '<br>';
+
+
+echo '6. ';
+
+echo '<br>'; echo '<br>'; echo '<br>';
+
+
+echo '7. ';
+echo '<br>'; echo '<br>'; echo '<br>';
+
+
+echo '8. ';
+// Reikia nupaišyti pilnavidurį rombą, taip pat, kaip ir pilnavidurį kvadratą, 
+// kurio aukštis 21 eilutė. Reikia padaryti, kad kiekviena rombo žvaigždutė būtų 
+// atsitiktinės RGB spalvos (perkrovus puslapį spalvos turi keistis).
+echo '<br>';
+function printDiamond($n) 
+{ 
+    $space = $n - 1; 
+
+    for ($i = 0; $i < $n; $i++) 
+    { 
+          
+        for ($j = 0;$j < $space; $j++) {
+            echo "&nbsp&nbsp";
+        }
+        for ($j = 0;$j <= $i; $j++) {
+            $color = str_pad(sprintf("%x%x%x", rand(0,255), rand(0,255), rand(0,255)),6,rand(0,9));
+            echo '<span style="Color:'.$color.'">'.'*&nbsp&nbsp'.'</span>';
+        }
+        echo '<br>';
+        $space--; 
+    } 
+  
+    $space = 0; 
+
+    for ($i = $n; $i > 0; $i--) 
+    { 
+          
+        for ($j = 0; $j < $space; $j++) {
+            // printf(" "); 
+            echo "&nbsp&nbsp";
+    }
+        for ($j = 0;$j < $i;$j++) { 
+            $color = str_pad(sprintf("%x%x%x", rand(0,255), rand(0,255), rand(0,255)),6,rand(0,9));
+            echo '<span style="Color:'.$color.'">'.'*&nbsp&nbsp'.'</span>';
+        } 
+        echo '<br>';
+        $space++; 
+    } 
+} 
+  printDiamond(10);
+
+
+
+echo '<br>'; echo '<br>'; echo '<br>';
+
+
+echo '9. ';
+echo '<br>'; echo '<br>'; echo '<br>';
+
+
+echo '10. ';
+
+
+
+
+
