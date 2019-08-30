efb43f6bac381572567920edf34e07f371f81278
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-08-30 16:28:14 +0300

    1

---
+++ Masyvai.php
@@ -0,0 +1,54 @@
+<?php
+echo '1.----------------------------------';
+// Sugeneruokite masyvą iš 30 elementų (indeksai nuo 0 iki 29),
+//  kurių reikšmės yra atsitiktiniai skaičiai nuo 5 iki 25.
+echo '<br>';
+$masyvas = [];
+for($i = 0; $i <= 29; $i++){
+$masyvas[] = rand(5, 25);
+}
+print_r($masyvas);
+
+echo '<br>'; echo '<br>'; echo '<br>';
+echo '2.----------------------------------';
+echo '<br>';
+
+
+echo '<br>'; echo '<br>'; echo '<br>';
+echo '3.----------------------------------';
+echo '<br>';
+
+
+echo '<br>'; echo '<br>'; echo '<br>';
+echo '4.----------------------------------';
+echo '<br>';
+
+
+echo '<br>'; echo '<br>'; echo '<br>';
+echo '5.----------------------------------';
+echo '<br>';
+
+
+echo '<br>'; echo '<br>'; echo '<br>';
+echo '6.----------------------------------';
+echo '<br>';
+
+
+echo '<br>'; echo '<br>'; echo '<br>';
+echo '7.----------------------------------';
+echo '<br>';
+
+
+echo '<br>'; echo '<br>'; echo '<br>';
+echo '8.----------------------------------';
+echo '<br>';
+
+
+echo '<br>'; echo '<br>'; echo '<br>';
+echo '9.----------------------------------';
+echo '<br>';
+
+
+echo '<br>'; echo '<br>'; echo '<br>';
+echo '10.----------------------------------';
+echo '<br>';
